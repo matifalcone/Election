0ac4d6099f18612a6a4b69eec317646b3fa3250e
Matias Falcone <matias.falcone@gmail.com>
2020-05-09 16:49:04 +1000

    1. Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.16;
+
+contract Election {
+	// Store Candidate
+	// Read Candidate
+	string public candidate;
+	// Constructor
+	constructor () public {
+		// State Variable
+		candidate = "Candidate 1";
+	}
+
+}
